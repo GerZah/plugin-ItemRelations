cd7ec4940bf57c69663bb8f51fd7c7d22450bcf7
Daniel Berthereau <Daniel.github@Berthereau.net>
2016-11-28 00:00:00 +0100

    Added a method to find relations simpler.

--- models/Table/ItemRelationsRelation.php
+++ models/Table/ItemRelationsRelation.php
@@ -106,4 +106,35 @@ class Table_ItemRelationsRelation extends Omeka_Db_Table
         }
         return $this->fetchObjects($select);
     }
+
+    /**
+     * Find all specified relations.
+     *
+     * @internal This is a short for findBy().
+     *
+     * @param integer|array $objectItemId
+     * @param integer|array $propertyId
+     * @param integer|array $subjectItemId
+     * @return array
+     */
+    public function findRelations($subjectItemId = null, $propertyId = null, $objectItemId = null)
+    {
+        $params = array();
+        if (!is_null($subjectItemId)) {
+            $params['subject_item_id'] = is_array($subjectItemId)
+                ? array_map('intval', $subjectItemId)
+                : (integer) $subjectItemId;
+        }
+        if (!is_null($propertyId)) {
+            $params['property_id'] = is_array($propertyId)
+                ? array_map('intval', $propertyId)
+                : (integer) $propertyId;
+        }
+        if (!is_null($objectItemId)) {
+            $params['object_item_id'] = is_array($objectItemId)
+                ? array_map('intval', $objectItemId)
+                : (integer) $objectItemId;
+        }
+        return $this->findBy($params);
+    }
 }
